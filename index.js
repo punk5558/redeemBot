deda2fe589100b876475dac90c089a59d05e3c4b
punk5558 <hngqingheng@gmail.com>
2022-08-05 21:49:11 +0800

    updated

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const erc20Abi = require('./erc20Abi.json');
 const contractAddress = '0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000';
 const agoraContractAddress = '0xcFd482DcE13cA1d27834D381AF1b570E9E6C6810';
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+//const fetch = require('node-fetch');
+//const axios = require('axios').default;
 
 const mnemonicPhrase = process.env.MNEMONIC; // 12 word mnemonic
 let provider = new HDWalletProvider({
@@ -14,12 +16,12 @@ let provider = new HDWalletProvider({
   },
   providerOrUrl: process.env.HTTP_RPC
 });
-
+/*
 const network = {
   name: "andromeda",
   chainId: 1088,
   //_defaultProvider: (providers) => new providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/33PDDfQl0HEjXtuMlsr6yA6d4i5LmTk3")
-};
+};*/
 
 const web3 = new Web3(provider);
 //console.log(abi);
@@ -27,65 +29,41 @@ const provider1 = new ethers.providers.JsonRpcProvider(process.env.HTTP_RPC);
 const cEthContract = new ethers.Contract(contractAddress, erc20Abi, provider1);
 const cEthContractCopy = new web3.eth.Contract(abi, agoraContractAddress);
 
+
 console.log("bot listening")
 
-try{
-    run();
-}catch(err) {
-  console.error(err);
-}
+async function run(_amount){
+    // call the contract to redeemUnderlying
 
-async function run(){
-    while(true){
-      let bal = await cEthContract.balanceOf('0xcFd482DcE13cA1d27834D381AF1b570E9E6C6810');
-      bal = parseInt(ethers.BigNumber.from(bal).toString());
-
-      if(bal > 49000000000000000) {
-        try{
-          await cEthContractCopy.methods.redeemUnderlying(bal).send({
-              from: '0xc346293aaC7F51a37bCd4C589AB6e7a374A620a0',
-              gasLimit: '2000000',
-              //gasPrice: '25000000000',
-              });
-          } catch(err){
-            console.error(err);
-          }
-        console.log("redeem method called");
-        }
-        //console.log("end loop");
+    try{
+      await cEthContractCopy.methods.redeemUnderlying(_amount).send({
+          from: '0xc346293aaC7F51a37bCd4C589AB6e7a374A620a0',
+          gasLimit: '2000000',
+          gasPrice: '20000000000',
+          });
+      } catch(err){
+        console.error(err);
       }
+    console.log("redeem method called");
+
 }
 
+cEthContract.on("Transfer", (...parameters) =>{
 
-/*cEthContract.on("Transfer", (...parameters) =>{
+  //console.log(parameters[1], ethers.BigNumber.from(parameters[2]).toString());
 
   // once we found a tx with mint event, we are going to get the object and
   // check the method and mintAmount, which we will input into redeemUnderlying
-  async function run(_to, _amount){
-
-    if( _amount > 49000000000000000 && _to=="0xcFd482DcE13cA1d27834D381AF1b570E9E6C6810"){ // if method is mint as there are other methods that emit a mint event
-
-      // call the contract to redeemUnderlying
-
-      var inputAmount = ethers.BigNumber.from(_amount).toString();
-      //console.log("found event", inputAmount);
-      try{
-        await cEthContractCopy.methods.redeemUnderlying(inputAmount).send({
-            from: '0xc346293aaC7F51a37bCd4C589AB6e7a374A620a0',
-            gasLimit: '2000000',
-            //gasPrice: '25000000000',
-            });
-        } catch(err){
-          console.error(err);
-        }
-      console.log("redeem method called");
-    }
-  }
 
   try{
-      run(parameters[1], parameters[2]);
+    var inputAmount = ethers.BigNumber.from(parameters[2]).toString();
+    if(parameters[1]==agoraContractAddress && inputAmount > 49000000000000000){
+      run(inputAmount);
+    }
   }catch(err) {
     console.error(err);
   }
 
-});*/
+
+
+});
